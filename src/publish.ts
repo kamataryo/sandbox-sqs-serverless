99b5811b92aa79677127c9e61bb313e981b89e4c
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-03-19 17:22:40 +0900

    Some fix

--- src/publish.ts
+++ src/publish.ts
@@ -1,18 +1,19 @@
 import * as Lambda from "aws-lambda";
 import AWS from "aws-sdk";
 
+const sqs = new AWS.SQS();
 const { QUEUE_NAME, AWS_REGION } = process.env as {
   QUEUE_NAME: string;
   AWS_REGION: string;
 };
 
 export const handler = async (
-  event: Lambda.APIGatewayEvent,
-  _1: any,
+  _0: any,
+  context: Lambda.Context,
   callback: Lambda.Callback
 ) => {
-  const QUEUE_URL = `https://sqs.${AWS_REGION}.amazonaws.com/${event.requestContext.accountId}/${QUEUE_NAME}`;
-  const sqs = new AWS.SQS();
+  const ACCOUNT_ID = context.invokedFunctionArn.split(":")[4];
+  const QUEUE_URL = `https://sqs.${AWS_REGION}.amazonaws.com/${ACCOUNT_ID}/${QUEUE_NAME}`;
 
   const message = await sqs
     .sendMessage({ MessageBody: "hoge", QueueUrl: QUEUE_URL })
